76a630d5b0ea0bd209ef2443ff07182593bad442
Mds-pixel <miftahulfirdaus776@gmail.com>
2023-04-03 20:59:53 +0700

    upload pertemuan 2

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo "<h1>Assalamualaikum wr.wb.</h1>";
+		echo "<h2>Hai sobat,Perkenalkan</h2>";
+		echo "<h3>";
+ echo"Nama saya Miftahul Firdaus
+ Saya tingga di daerah Otistaa
+ Salam kenal ya !!";
+ 
+	}
+}
